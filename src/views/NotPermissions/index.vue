c6c241983c797229387110841a905aea774b6a54
chenyc <chenyc@gzsendi.cn>
2025-07-17 10:35:02 +0800

    feat: 新增无权限页面

---
+++ src/views/NotPermissions/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="not-permissions">
+        <img src="@/assets/images/not_permissions.svg" alt="" />
+        <div class="not-permissions_text">暂无权限</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.not-permissions {
+	width: 100%;
+	height: 100%;
+	@include flex-center(column);
+
+	img {
+		object-fit: contain;
+		width: 400px;
+	}
+    
+    .not-permissions_text {
+        margin: $margin-base;
+        color: $gray-dark;
+        font-size: 22px;
+    }
+}
+</style>
