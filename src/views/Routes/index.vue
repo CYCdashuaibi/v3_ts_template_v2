68316daa26a5f94402b518fca7f60b1d8d44f51c
chenyc <chenyc@gzsendi.cn>
2025-07-18 14:54:10 +0800

    feat: 新增路由页面

---
+++ src/views/Routes/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="routes">
+		<div class="routes-item" v-for="route in routes" :key="route.path">
+			<router-link :to="route.path">{{ route.meta?.title ? `${route.meta?.title} -- `: '' }}{{ route.path }}</router-link>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import router from '@/router';
+
+const filterRoute = ['/', '/routes', '/:pathMatch(.*)*', '/not-permissions'];
+
+const routes = router.options.routes.filter(
+	(item) => !filterRoute.includes(item.path),
+);
+</script>
+
+<style scoped lang="scss">
+.routes {
+    padding: 20px;
+
+    .routes-item {
+        @include link-status();
+        padding: 5px 0;
+    }
+}
+</style>
